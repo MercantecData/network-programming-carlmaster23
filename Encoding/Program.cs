93e86a5703cf89a97664fab9e21dc7c6e3f1e9e9
Jonas <jonasarvad2@gmail.com>
2020-05-25 09:09:53 +0200

    Code and gitignor added

---
+++ Encoding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Jeg håber du er glad";
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            foreach 
+
+            
+        }
+    }
+}
