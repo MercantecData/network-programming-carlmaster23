a75a97afa881936336dc4d4d90c431b38632a10d
Jonas <jonasarvad2@gmail.com>
2020-05-25 13:11:26 +0200

    added klient and server to github

---
+++ Klient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Klient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            int port = 13356;
+            IPAddress ip = IPAddress.Parse("172.16.113.68");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+
+            string text = "Hello World!";
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 13356;
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint localEndpoint = new IPEndPoint(ip, port);
+
+            TcpListener listener = new TcpListener(localEndpoint);
+            listener.Start();
+
+            Console.WriteLine("Awaiting Clients");
+            TcpClient client = listener.AcceptTcpClient();
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[256];
+
+            int numberOfBytesRead = stream.Read(buffer, 0, 256);
+
+            string message = Encoding.UTF8.GetString(buffer, 0,
+            numberOfBytesRead);
+
+            Console.WriteLine(message);
+        }
+    }
+}
