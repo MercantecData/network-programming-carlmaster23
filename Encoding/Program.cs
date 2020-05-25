74d82f7b5d4ccf728c712aceed30e8dc8b17994f
Jonas <jonasarvad2@gmail.com>
2020-05-25 10:27:23 +0200

    Code update

--- Encoding/Program.cs
+++ Encoding/Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Text;
 
-namespace Encoding
+namespace Encoding2
 {
     class Program
     {
         static void Main(string[] args)
         {
             string text = "Jeg håber du er glad";
+            string text2 = "Jeg øver mig lige lidt";
+
+            byte[] bytes2 = Encoding.ASCII.GetBytes(text2);
             byte[] bytes = Encoding.UTF8.GetBytes(text);
 
-            foreach 
+            foreach(byte i in bytes2)
+            {
+                Console.WriteLine(i);
+            }
 
-            
+            foreach(byte i in bytes)
+            {
+                Console.WriteLine(i);
+            }
+
+            string converted2 = Encoding.ASCII.GetString(bytes2);
+
+            string converted = Encoding.UTF8.GetString(bytes);
+
+            Console.WriteLine(converted);
+
+            Console.WriteLine(converted2);
         }
     }
 }
