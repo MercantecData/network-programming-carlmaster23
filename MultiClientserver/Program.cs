b138408d7f38a81e91ea98bf5d0dcf3edf215869
Jonas <jonasarvad2@gmail.com>
2020-05-28 14:57:56 +0200

    added functionality to multiserver and added klients

--- MultiClientserver/Program.cs
+++ MultiClientserver/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
 
 namespace MultiClientserver
 {
@@ -6,7 +10,74 @@ namespace MultiClientserver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("serveren eller tryk på en knap for at lukke");
+            string statercode = Console.ReadLine();
+
+            if (statercode == "server")
+            {
+                MyServer Server = new MyServer();
+            }
+            else if(statercode == "klient")
+            {
+                klienter.klienterne();
+            }
+            else
+            {
+                Console.WriteLine("closing program");
+            }
+        }
+
+        public class MyServer
+        {
+            public List<TcpClient> clients = new List<TcpClient>();
+            public MyServer()
+            {
+                IPAddress ip = IPAddress.Parse("172.16.113.239");
+                int port = 5000;
+                TcpListener listener = new TcpListener(ip, port);
+                listener.Start();
+
+                AcceptClients(listener);
+
+                bool isRunning = true;
+                while (isRunning)
+                {
+                    // send a Message
+                    Console.Write("Write message: ");
+                    string text = Console.ReadLine();
+                    byte[] buffer = Encoding.UTF8.GetBytes(text);
+                    
+                    
+                    foreach(TcpClient client in clients)
+                    {
+                        client.GetStream().Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+            public async void AcceptClients(TcpListener listener)
+            {
+                bool isRunning = true;
+                while(isRunning)
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    clients.Add(client);
+                    NetworkStream stream = client.GetStream();
+                    ReceiveMessages(stream);
+
+                }
+
+            }
+        public async void ReceiveMessages(NetworkStream stream) {
+                byte[] buffer = new byte[256];
+                bool IsRunning = true;
+                while (IsRunning)
+                {
+                    int read = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    string text = Encoding.UTF8.GetString(buffer, 0, read);
+                    Console.WriteLine("client writes: " + text);
+                }
+            }
+        
         }
     }
 }
---
+++ MultiClientserver/klients.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace MultiClientserver
+{
+    class klienter
+    {
+        public static void klienterne()
+        {
+            int portnummer;
+            string text;
+            // int beskedfraserver;
+            // string serverbeskedlæsning;
+            string ipadresse;
+
+            TcpClient client = new TcpClient();
+
+            Console.WriteLine("Skriv portnummeret");
+            portnummer = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Skriv IPadressen");
+            ipadresse = Console.ReadLine();
+
+            IPAddress ip = IPAddress.Parse(ipadresse);
+            IPEndPoint endPoint = new IPEndPoint(ip, portnummer);
+
+            client.Connect(endPoint);
+            bool kør = true;
+            while (true)
+                if (kør == true)
+                {
+                    NetworkStream stream = client.GetStream();
+                    Console.WriteLine("Skriv en besked til serveren");
+                    text = Console.ReadLine();
+                    byte[] buffer = Encoding.UTF8.GetBytes(text);
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            else
+                { 
+                    client.Close();
+                }
+            
+        }
+    }
+}
