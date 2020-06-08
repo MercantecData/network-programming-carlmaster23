ebc4408d0fd907d09ff71080311b210aa87c1f4b
Jonas <jonasarvad2@gmail.com>
2020-06-08 14:41:30 +0200

    spilt server and klient so this is only server

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Text;
 
 namespace Server
-
 {
     class Program
     {
@@ -13,104 +12,13 @@ namespace Server
             Console.WriteLine("Hvad vil du gerne være serveren eller klienten");
             string nummer = Console.ReadLine();
 
-
-            if (nummer == "klienten")
+            if (nummer == "serveren")
             {
-                klient();
-            }
-            else if (nummer == "serveren")
-            {
-                server();
+                MyServer server = new MyServer();
             }
             else
             {
                 Console.WriteLine("Du skrev noget forkert");
-            }
-
-
-            static void klient()
-            {
-                int portnummer;
-                string text;
-                // int beskedfraserver;
-                // string serverbeskedlæsning;
-                string ipadresse;
-
-                TcpClient client = new TcpClient();
-
-                Console.WriteLine("Skriv portnummeret");
-                portnummer = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Skriv IPadressen");
-                ipadresse = Console.ReadLine();
-
-                IPAddress ip = IPAddress.Parse(ipadresse);
-                IPEndPoint endPoint = new IPEndPoint(ip, portnummer);
-
-                client.Connect(endPoint);
-
-                NetworkStream stream = client.GetStream();
-
-                Console.WriteLine("Skriv en besked til serveren");
-                text = Console.ReadLine();
-
-                byte[] buffer = Encoding.UTF8.GetBytes(text);
-
-                stream.Write(buffer, 0, buffer.Length);
-
-                /*
-                TcpListener fåbeskendtilbage = new TcpListener(endPoint);
-                fåbeskendtilbage.Start();
-                Console.WriteLine("Awaiting Clients");
-                TcpClient server2 = fåbeskendtilbage.AcceptTcpClient();
-                NetworkStream klientsvares = server2.GetStream();
-                byte[] serverbesked = new byte[256];
-                beskedfraserver = stream.Read(serverbesked, 0, 256);
-                serverbeskedlæsning = Encoding.UTF8.GetString(serverbesked, 0, beskedfraserver);
-                Console.WriteLine(serverbeskedlæsning);
-                */
-                client.Close();
-            }
-
-
-            static void server()
-            {
-                // the parameters that vil be used in the code
-                int portnummer;
-                int numberOfBytesRead;
-                string message;
-                // string beskeden;
-
-                Console.WriteLine("Skriv portnummer");
-                portnummer = Convert.ToInt32(Console.ReadLine());
-
-                IPAddress ip = IPAddress.Any;
-                IPEndPoint localEndpoint = new IPEndPoint(ip, portnummer);
-
-                TcpListener listener = new TcpListener(localEndpoint);
-                listener.Start();
-
-                Console.WriteLine("Awaiting Clients");
-                TcpClient client = listener.AcceptTcpClient();
-
-                NetworkStream stream = client.GetStream();
-
-                byte[] buffer = new byte[256];
-
-                numberOfBytesRead = stream.Read(buffer, 0, 256);
-
-                message = Encoding.UTF8.GetString(buffer, 0,
-                numberOfBytesRead);
-
-                Console.WriteLine(message);
-                /*
-                TcpClient server2 = new TcpClient();
-                server2.Connect(localEndpoint);
-                NetworkStream klientsvares = server2.GetStream();
-                beskeden = "Tak for din tid";
-                byte[] beskedtilklient = Encoding.UTF8.GetBytes(beskeden);
-                stream.Write(beskedtilklient, 0, beskedtilklient.Length);
-                */
             }
         }
     }
---
+++ Server/Serversite.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+        public class MyServer
+        {
+            public MyServer()
+            {
+                // the parameters that vil be used in the code
+                int portnummer;
+                int numberOfBytesRead;
+                string message;
+                // string beskeden;
+
+                Console.WriteLine("Skriv portnummer");
+                portnummer = Convert.ToInt32(Console.ReadLine());
+
+                IPAddress ip = IPAddress.Any;
+                IPEndPoint localEndpoint = new IPEndPoint(ip, portnummer);
+
+                TcpListener listener = new TcpListener(localEndpoint);
+                listener.Start();
+
+                Console.WriteLine("Awaiting Clients");
+                TcpClient client = listener.AcceptTcpClient();
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[256];
+
+                numberOfBytesRead = stream.Read(buffer, 0, 256);
+
+                message = Encoding.UTF8.GetString(buffer, 0,
+                numberOfBytesRead);
+
+                Console.WriteLine(message);
+
+                byte[] svartilbage = Encoding.UTF8.GetBytes("Det er modtaget" + "\n");
+                client.GetStream().Write(svartilbage, 0, svartilbage.Length);
+
+            }
+        }
+    }
