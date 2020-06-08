5e5d54e3b7f41808c0c59f3f47dabadf38295af7
Jonas <jonasarvad2@gmail.com>
2020-06-08 14:50:05 +0200

    added project
    added a more complete version of the first assignment

---
+++ Server and Client first asignment/Klient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server_and_Client_first_asignment
+{
+    public class Klienten
+    {
+        public Klienten()
+        {
+            string text;
+
+            TcpClient client = new TcpClient();
+
+            IPAddress IP = IPAddress.Parse("127.0.0.1");
+
+            IPEndPoint endPoint = new IPEndPoint(IP, 5000);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("Skriv en besked til serveren");
+
+            text = Console.ReadLine();
+
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            ReceiveMessage(stream);
+
+            client.Close();
+        }
+        public static void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+            int numberOfBytesRead = stream.Read(buffer, 0, 256);
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+
+            Console.Write("Server message: " + receivedMessage + "\n");
+        }
+    }
+}
---
+++ Server and Client first asignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server_and_Client_first_asignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hvad vil du gerne være serveren eller klienten");
+            string svar = Console.ReadLine();
+
+            if (svar == "serveren")
+            {
+                MyServer server = new MyServer();
+            }
+            else if(svar == "klienten")
+            {
+                Klienten klient = new Klienten();
+            }
+            else
+            {
+                Console.WriteLine("Du skrev noget forkert");
+            }
+        }
+    }
+}
