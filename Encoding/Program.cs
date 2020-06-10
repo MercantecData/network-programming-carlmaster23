fb66b663c301952b45cc8b86b8f3fd6098617aee
Jonas <jonasarvad2@gmail.com>
2020-06-10 14:37:06 +0200

    comments to code

--- Encoding/Program.cs
+++ Encoding/Program.cs
@@ -7,28 +7,32 @@ namespace Encoding2
     {
         static void Main(string[] args)
         {
+            // defining some string message for later use
             string text = "Jeg håber du er glad";
             string text2 = "Jeg øver mig lige lidt";
 
+            // changing message from letters to bytes
             byte[] bytes2 = Encoding.ASCII.GetBytes(text2);
             byte[] bytes = Encoding.UTF8.GetBytes(text);
 
+            // printing the bytes to console
             foreach(byte i in bytes2)
             {
                 Console.WriteLine(i);
             }
 
-            foreach(byte i in bytes)
+            // printing the bytes to console
+            foreach (byte i in bytes)
             {
                 Console.WriteLine(i);
             }
 
+            // changing the message from bytes to letters
             string converted2 = Encoding.ASCII.GetString(bytes2);
-
             string converted = Encoding.UTF8.GetString(bytes);
 
+            // printing the Encoding message
             Console.WriteLine(converted);
-
             Console.WriteLine(converted2);
         }
     }
